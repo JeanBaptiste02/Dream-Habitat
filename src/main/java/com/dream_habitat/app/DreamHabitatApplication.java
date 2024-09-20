c0be72ff748176de49d504fdb61b4485d1d37be1
DAMODARANE Jean-Baptiste <91695685+JeanBaptiste02@users.noreply.github.com>
2024-09-20 09:16:52 +0200

    Initial commit

---
+++ src/main/java/com/dream_habitat/app/DreamHabitatApplication.java
@@ -0,0 +1,13 @@
+package com.dream_habitat.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamHabitatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamHabitatApplication.class, args);
+	}
+
+}
