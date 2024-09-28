6df89dcf9f92c06da1342299f0326824164ca491
Kamel <kamel.talatizi@planzone.com>
2024-09-28 10:35:11 +0200

    feat: add User model with Lombok annotations

---
+++ src/main/java/com/dream_habitat/app/model/User.java
@@ -0,0 +1,27 @@
+package com.dream_habitat.app.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Table(name = "app_user")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+}
