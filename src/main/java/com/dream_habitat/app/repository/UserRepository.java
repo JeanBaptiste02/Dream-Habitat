01cae00cba6203d5dc5581cc8dbcb6b225aa1e07
Kamel <kamel.talatizi@planzone.com>
2024-09-28 10:41:29 +0200

    feat: add User repository

---
+++ src/main/java/com/dream_habitat/app/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.dream_habitat.app.repository;
+
+
+import com.dream_habitat.app.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
+
