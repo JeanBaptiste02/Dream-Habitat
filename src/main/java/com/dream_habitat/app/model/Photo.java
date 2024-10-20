71c7f3a1ca85566818c1ad01af56860e13df143a
Kamel <kamel.talatizi@planzone.com>
2024-10-21 00:33:30 +0200

    added photo model

---
+++ src/main/java/com/dream_habitat/app/model/Photo.java
@@ -0,0 +1,46 @@
+package com.dream_habitat.app.model;
+
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+/**
+ * Entity class representing a photo
+ * @author Jean-Baptiste, Kamel, Victor
+ */
+@Entity
+@Table(name = "photo")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Photo implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "path")
+    private String path;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "date")
+    private LocalDateTime date;
+
+    @ManyToOne
+    @JoinColumn(name = "room_id", nullable = false)
+    private Room room;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private User owner;
+
+
+}
