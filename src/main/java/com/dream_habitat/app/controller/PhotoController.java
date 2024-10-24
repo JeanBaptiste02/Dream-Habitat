b1f3a4a6c89d1201e91538980b8b271d6b62d643
Kamel <kamel.talatizi@planzone.com>
2024-10-24 12:16:21 +0200

    feat: encode file URLs before saving to ensure special character handling

--- src/main/java/com/dream_habitat/app/controller/PhotoController.java
+++ src/main/java/com/dream_habitat/app/controller/PhotoController.java
@@ -17,11 +17,12 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("api/photo")
@@ -40,24 +41,12 @@ public class PhotoController {
     private RoomService roomService;
 
 
-    /**
-     * Uploads a photo to the server
-     * @param file The MultipartFile containing the photo data
-     * @param name The name of the photo
-     * @param description The description of the photo
-     * @param roomId The ID of the album to which the photo belongs
-     * @return ResponseEntity containing a success message or an error message and HTTP status
-     */
     @PostMapping("/upload")
     public ResponseEntity<String> uploadPhoto(@RequestParam("file") MultipartFile file,
                                               @RequestParam("name") String name,
                                               @RequestParam("description") String description,
                                               @RequestParam("roomId") Room roomId,
-
                                               @RequestHeader(value="Authorization") String token) {
-
-
-
 
         if (token != null) {
             token = token.substring(7);
@@ -66,26 +55,33 @@ public class PhotoController {
                 LocalDateTime date = LocalDateTime.now();
                 String currentDirectory = System.getProperty("user.dir");
 
+                // Chemin de base pour les photos
                 String photoDirPath = currentDirectory + File.separator + "photosData";
                 File photoDir = new File(photoDirPath);
                 if (!photoDir.exists()) {
                     photoDir.mkdirs();
                 }
 
+                // Crée un dossier pour chaque utilisateur
                 String ownerIdDirPath = photoDirPath + File.separator + existingUser.getName();
                 File ownerIdDir = new File(ownerIdDirPath);
                 if (!ownerIdDir.exists()) {
                     ownerIdDir.mkdirs();
                 }
 
+                // Enregistrer le fichier
                 String fileName = file.getOriginalFilename();
                 Path filePath = Paths.get(ownerIdDirPath, fileName);
                 file.transferTo(filePath.toFile());
 
-                String myFilePath = File.separator + "photosData" + File.separator + existingUser.getName() + File.separator + fileName;
+                // Encodage de l'URL pour les caractères spéciaux
+                String encodedFileName = URLEncoder.encode(fileName, StandardCharsets.UTF_8.toString());
 
+                // URL encodée pour être stockée dans la base de données
+                String myFilePath = File.separator + "photosData" + File.separator + existingUser.getName() + File.separator + encodedFileName;
+
+                // Création de l'objet Photo et enregistrement
                 User user = userService.getUserByEmail(existingUser.getEmail()).get();
-
                 Photo photo = new Photo(fileName, myFilePath, description, date, roomId, user);
                 photoService.savePhoto(photo);
 
@@ -94,10 +90,11 @@ public class PhotoController {
                 e.printStackTrace();
                 return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to upload photo");
             }
-        }else {
+        } else {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized");
         }
     }
+
 
     @GetMapping("/room/{roomId}")
     public ResponseEntity<List<Photo>> getPhotosByRoomId(@PathVariable Long roomId) {
